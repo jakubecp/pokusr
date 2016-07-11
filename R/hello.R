86249f2960ecc1061e591a3b6facd980b78b9f50
jakubecp <jakubecp@fzp.czu.cz>
2016-07-11 22:21:29 +0200

    playing with fields. added license

--- R/hello.R
+++ R/hello.R
@@ -13,14 +13,28 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
-}
-install.packages("dplyr")
-devtools::use_package("dplyr") # Defaults to imports
-#> Adding dplyr to Imports
-#> Refer to functions with dplyr::fun()
-devtools::use_package("dplyr", "Suggests")
-#> Adding dplyr to Suggests
+# hello <- function() {
+#   print("Hello, world!")
+# }
+# # install.packages("dplyr")
+# # devtools::use_package("dplyr") # Defaults to imports
+# #> Adding dplyr to Imports
+# #> Refer to functions with dplyr::fun()
+# devtools::use_package("dplyr", "Suggests")
+# #> Adding dplyr to Suggests
 #> Use requireNamespace("dplyr", quietly = TRUE) to test if package is
 #> installed, then use dplyr::fun() to refer to functio
+
+#ROXYGEN2 example of syntax
+#' Add together two numbers.
+#'
+#' @param x A number.
+#' @param y A number.
+#' @return The sum of \code{x} and \code{y}.
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
+add <- function(x, y) {
+  x + y
+}
+devtools::document()
