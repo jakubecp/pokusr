8cef1646b5b70b225b36fe9fe17ac1dcdb242dbf
jakubecp <jakubecp@fzp.czu.cz>
2016-07-18 21:12:25 +0200

    dir structure is not needed anymore

--- R/dir_structure.R
+++
@@ -1,9 +0,0 @@
-# package standard dir structure
-dir.create ("src")
-dir.create ("tests")
-dir.create ("vignettes")
-dir.create ("inst")
-dir.create ("inst/templates")
-dir.create ("R")
-dir.create ("man")
-
