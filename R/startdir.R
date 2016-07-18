d399cf57d4d0faf24b50dff3b9403c8c721ced97
jakubecp <jakubecp@fzp.czu.cz>
2016-07-18 17:14:26 +0200

    R is now not included in package options, because is created by Rstudio
    by default

--- R/startdir.R
+++ R/startdir.R
@@ -2,10 +2,10 @@
 
 startdir <- function (type=c("package", "analysis")) {
   if (type=="package") {
-    package <- c("src","tests", "vignettes","inst", "inst/templates", "R", "man")
+    package <- c("src","tests", "vignettes","inst", "inst/templates", "man")
     sapply (package, dir.create)
   } else if (type== "analysis") {
-    analysis <- c("articl","data", "exports","Ra")
+    analysis <- c("articl","data", "exports","R")
     sapply (analysis, dir.create)
   } else {
     print ("ERROR")
--- R/undir.R
+++ R/undir.R
@@ -1,9 +1,12 @@
+#function for deleting dir structure created by function startdir
+
+
 undir <- function (type=c("package", "analysis")) {
   if (type == "package") {
-    package <- c("src","tests", "vignettes","inst", "inst/templates", "R", "man")
+    package <- c("src","tests", "vignettes","inst", "inst/templates", "man")
     sapply (package, unlink, recursive=TRUE)
   } else if (type== "analysis") {
-    analysis <- c("articl","data", "exports","Ra")
+    analysis <- c("articl","data", "exports","R")
     sapply (analysis, unlink, recursive=TRUE)
   } else {
     print ("ERROR")
